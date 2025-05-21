ac3b9770280fc3d92012d3467d30e4932dc94a55
alecarejola00 <45085978+alecarejola00@users.noreply.github.com>
2025-05-21 23:35:50 +0800

    ENV update
    update to backend(not frontend link)

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://charthub-frontend.onrender.com'  // backend URL in Render
+  apiUrl: 'https://charthub-backend.onrender.com'  // backend URL in Render
 };
